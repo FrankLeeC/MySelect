970d57d95238e4d2067da91da23c63621a3de32d
frank <603128423@qq.com>
2016-08-16 10:43:29 +0800

    branch

--- src/com/lwy/myselect/session/SimpleSession.java
+++ src/com/lwy/myselect/session/SimpleSession.java
@@ -139,17 +139,7 @@ public final class SimpleSession extends BaseSession{
 	public void addBatch(){
 		
 	}
-	
-//	@SuppressWarnings("unchecked")
-//	private int singleSqlBatch(String sql,Object object){
-//		if(!isTransaction())
-//			return insertAndDeleteAndUpdate(sql, object);
-//		else{
-//			singleBatchSql = sql;
-//			batchObjectList = (List<Object>) object;
-//			return batchObjectList.size();
-//		}
-//	}
+
 
 	/**
 	 * put in cache after insert
@@ -179,15 +169,6 @@ public final class SimpleSession extends BaseSession{
 		return result;
 	}
 
-//	@Override
-//	public int delete(String sql,Object object){
-//		return singleSqlBatch(sql,object);
-//	}
-//
-//	@Override
-//	public int update(String sql,Object object){
-//		return singleSqlBatch(sql,object);
-//	}
 
 	@Override
 	public int delete(String sql,Object object){
@@ -234,73 +215,6 @@ public final class SimpleSession extends BaseSession{
 		return 0;
 	}
 
-//	private int insertAndDeleteAndUpdate(String sql,Object object){
-//		int result = 0;
-//		EntityMapper entityMapper = configuration.getEntity(clazz.getName());
-//		String sqlStatement = entityMapper.getSQLMapper(sql).getSql();
-//		List<Object> propertyList = null;
-//		List<List<Object>> propertyBatch = null;
-//		PreparedStatement ps = null;
-//		if(!isTransaction()){
-//			propertyList = Reflection.reflectToGetProperty(sqlStatement, object, clazz,entityMapper);
-//		}
-//		else{
-//			propertyBatch = new ArrayList<>();
-//			for(int i=0;i<batchObjectList.size();i++){
-//				List<Object> property = Reflection.reflectToGetProperty(sqlStatement, batchObjectList.get(i)
-//																						, clazz,entityMapper);
-//				propertyBatch.add(property);
-//			}
-//		}
-//		if(connection != null){
-//			try {
-//				if(!isTransaction()){
-//					ps = connection.prepareStatement(sqlStatement);
-//					for(int i=0;i<propertyList.size();i++)
-//						ps.setObject(i+1, propertyList.get(i));
-//					result = ps.executeUpdate();
-//				}
-//				else{
-//					connection.setAutoCommit(false);
-//					ps = connection.prepareStatement(sqlStatement);
-//					for(int j=0;j<batchObjectList.size();j++){
-//						List<Object> properties = propertyBatch.get(j);
-//						for(int i=0;i<properties.size();i++)
-//							ps.setObject(i+1, properties.get(i));
-//						ps.addBatch();
-//						if(j>0&&j%200 == 0){
-//							ps.executeBatch();
-//							connection.commit();
-//							ps.clearBatch();
-//						}
-//					}
-//					ps.executeBatch();
-//					ps.clearBatch();
-//					connection.commit();
-//					connection.setAutoCommit(true);
-//					result = batchObjectList.size();
-//				}
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//				if(isTransaction()){
-//					try {
-//						if(connection != null){
-//							connection.rollback();
-//							connection.setAutoCommit(true);
-//						}
-//					} catch (Exception e2) {
-//						e2.printStackTrace();
-//					}
-//				}
-//			}finally{
-//				if(isTransaction())
-//					resetBatch(); //重置batchList 和  sql
-//				if(ps != null)
-//					ConnectionPool.closeConnection(connection, ps);
-//			}
-//		}
-//		return result;
-//	}
 
 	/**
 	 * find in cache
@@ -369,76 +283,7 @@ public final class SimpleSession extends BaseSession{
 		}
 		return null;
 	}
-	
-//	@Override
-//	public Object select(String sql,Object object){
-//		EntityMapper entityMapper = configuration.getEntity(clazz.getName());
-//		String sqlStatement = entityMapper.getSQLMapper(sql).getSql();
-//		if(!sqlStatement.contains("count(*)")){
-//			Object result = findInCache(object,entityMapper);
-//			if(result != null) {
-//				System.out.println("get in cache");
-//				return result;
-//			}
-//		}
-//		String returnAlias = entityMapper.getSQLMapper(sql).getReturnAlias();
-//		List<Object> list = Reflection.reflectToGetProperty(sqlStatement, object, clazz,entityMapper);
-//		List<Object> result = new ArrayList<>();
-//		PreparedStatement ps = null;
-//		ResultSet rs = null;
-//		int len = list.size();
-//		List<String> columnList = null;
-//		int columnLen = 0;
-//		//带条件查询
-//		if(len>0){
-//			String className = configuration.getClassName(returnAlias);    //返回对象的类名
-//			EntityMapper em = configuration.getEntity(className);          //返回对象的实体映射
-//			columnList = Reflection.reflectToGetColumn(sqlStatement,em);   //返回的字段名
-//			columnLen = columnList.size();
-//		}
-//		if(connection != null){
-//			try {
-//				ps = connection.prepareStatement(sqlStatement);
-//				//如果带条件查询，就需要填充
-//				if(len>0){
-//					for(int i=0;i<len;i++)
-//						ps.setObject(i+1, list.get(i));
-//				}
-//				rs = ps.executeQuery();
-//				//如果查询数量
-//				if(sqlStatement.contains("count(*)")){
-//					rs.first();
-//					return rs.getObject("count(*)"); //查询记录条数时，结果是一张表，count(*)字段就是记录条数
-//				}
-//				//如果不是查询数量
-//				String className = configuration.getClassName(returnAlias);
-//				while(rs.next()){
-//					List<Object> columnResult = new ArrayList<>();
-//					for(int i=0;i<columnLen;i++){
-//						columnResult.add(rs.getObject(columnList.get(i)));
-//					}
-//					EntityMapper em = configuration.getEntity(className);
-//					Object o = Reflection.reflectToCreateEntity(columnResult, className, columnList,em);
-//					Object id = Reflection.reflectToGetId(em,o);
-//					//如果id属性不为空，则将其缓存起来
-//					if(id != null) {
-//						sessionFactory.cache(className, id, o);
-//					}
-//					result.add(o);
-//				}
-//				return result;
-//			} catch (SQLException e) {
-//				e.printStackTrace();
-//			} finally{
-//				if(rs != null){
-//					ConnectionPool.closeConnection(connection, ps, rs);
-//					connection = null;
-//					closed = true;
-//				}
-//			}
-//		}
-//		return null;
-//	}
+
 
 	protected static class Builder{
 
